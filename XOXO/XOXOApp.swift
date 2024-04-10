829532131b0b02e7fa303a3fc4a5f60c23e1dae3
wncelrcn <rivanowincelarcen@gmail.com>
2024-04-10 09:45:09 +0800

    Initial Commit

---
+++ XOXO/XOXOApp.swift
@@ -0,0 +1,17 @@
+//
+//  XOXOApp.swift
+//  XOXO
+//
+//  Created by Wince Larcen on 4/10/24.
+//
+
+import SwiftUI
+
+@main
+struct XOXOApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
